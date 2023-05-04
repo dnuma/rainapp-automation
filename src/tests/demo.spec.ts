ca080e09e95f40d5b4667e4c5b451188bd49c7ee
dnuma-eip <dnuma@evolveip.net>
2023-05-04 15:15:31 -0400

    Minor fixes, added 2 new tests

--- src/tests/demo.spec.ts
+++ src/tests/demo.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-import { URL_TEST, USERNAME, EMAIL, NAVIGATION, BUTTONS, TITLES } from "@config/constants"
+import { URL_TEST, USERNAME, EMAIL, NAVIGATION, BUTTONS, TITLES } from "@data/constants"
 import { BasePage } from "@pages/base.page";
 import { BlogPage } from "@pages/blog.page";
 import { UserData } from "@interfaces/userData";
@@ -18,8 +18,7 @@ test.describe('Navigate on RainApp website',
      * Navigates to each link in the navigation bar and evaluates that it is the current one.
      */
     test('Navigation test', async ({ page })  => {
-      const basePage = new BasePage(page);
-      const ariaCurrentAttribute = 'aria-current';
+      const basePage = new BasePage(page);      
       const selectedLinkAttribute = 'page';
 
       // Validate that all links in the nav bar are visible
@@ -31,47 +30,70 @@ test.describe('Navigate on RainApp website',
       expect.soft( await basePage.isNavTabVisible( BUTTONS.EMPLOYERS_ACCESS )).toBeTruthy();
 
       // Click on each link of the top bar and evaluate that is the current one
-      await basePage.nameInput.click();
-      expect.soft(await basePage.nameInput.getAttribute(ariaCurrentAttribute)).toMatch(selectedLinkAttribute);
+      await basePage.clickNavTab(NAVIGATION.ABOUT);
+      expect.soft(await basePage.getNavTabAttribute(NAVIGATION.ABOUT)).toMatch(selectedLinkAttribute);
       expect.soft(page).toHaveURL(URL_TEST.ABOUT);
 
-      await basePage.emailInput.click();
-      expect.soft(await basePage.emailInput.getAttribute(ariaCurrentAttribute)).toMatch(selectedLinkAttribute);
+      await basePage.clickNavTab(NAVIGATION.CAREERS);
+      expect.soft(await basePage.getNavTabAttribute(NAVIGATION.CAREERS)).toMatch(selectedLinkAttribute);
       expect.soft(page).toHaveURL(URL_TEST.CAREERS);
 
-      await basePage.navBlog.click();
-      expect.soft(await basePage.navBlog.getAttribute(ariaCurrentAttribute)).toMatch(selectedLinkAttribute);
+      await basePage.clickNavTab(NAVIGATION.BLOG);
+      expect.soft(await basePage.getNavTabAttribute(NAVIGATION.BLOG)).toMatch(selectedLinkAttribute);
       expect.soft(page).toHaveURL(URL_TEST.BLOG);
 
-      await basePage.navSupport.click();
-      expect.soft(await basePage.navSupport.getAttribute(ariaCurrentAttribute)).toMatch(selectedLinkAttribute);
+      await basePage.clickNavTab(NAVIGATION.SUPPORT);
+      expect.soft(await basePage.getNavTabAttribute(NAVIGATION.SUPPORT)).toMatch(selectedLinkAttribute);
       expect.soft(page).toHaveURL(URL_TEST.SUPPORT);      
+    });
+
+    /**
+     * Test to sign up for blog news on the blog page without checking the news checkbox
+     */
+      test('Sign up for blog news without checking the newsletter checkbox', async ({ page })  => {
+        const blogPage = new BlogPage(page);
+        const isReceiveNews = false;
+        const validUser: UserData = {
+          userName: `${USERNAME.FIRST_NAME} ${USERNAME.LAST_NAME}`,
+          email: EMAIL.VALID
+        };
+  
+        await blogPage.navBlog.click();
+        await blogPage.fillBlogForm( validUser, isReceiveNews );
+        expect.soft(page).toHaveURL(URL_TEST.BLOG);
+        expect( await blogPage.formSubmitted.isVisible(), 'No news checkbox checked' ).not.toBeTruthy();
+      });
+
+    /**
+     * Test to sign up for blog news on the blog page with valid email formats.
+     */
+    test('Sign up for blog news with valid email', async ({ page })  => {
+      const blogPage = new BlogPage(page);
+      const validUser: UserData = {
+        userName: `${USERNAME.FIRST_NAME} ${USERNAME.LAST_NAME}`,
+        email: EMAIL.VALID
+      };
+
+      await blogPage.navBlog.click();
+      await blogPage.fillBlogForm( validUser );
+      expect.soft(page).toHaveURL(URL_TEST.BLOG);
+      expect( await blogPage.formSubmitted.isVisible(), 'Valid email format' ).toBeTruthy();
     });
 
     /**
      * Test to sign up for blog news on the blog page with valid and invalid email formats.
      */
-    test('Sign up for blog news', async ({ page })  => {
+    test('Sign up for blog news with invalid email', async ({ page })  => {
       const blogPage = new BlogPage(page);
-      const isReceiveNews = true;
-      const validUser: UserData = {
-        userName: `${USERNAME.FIRST_NAME} ${USERNAME.LAST_NAME}`,
-        email: EMAIL.VALID
-      };
       const invalidUser: UserData = {
         userName: `${USERNAME.FIRST_NAME} ${USERNAME.LAST_NAME}`,
         email: EMAIL.INVALID
       };      
       
       await blogPage.navBlog.click();
-      expect(page).toHaveURL(URL_TEST.BLOG);
-
-      await blogPage.fillBlogForm( validUser, isReceiveNews );
-      expect.soft( await blogPage.formSubmitted.isVisible(), 'Valid email format' ).toBeTruthy();
-
-      await page.reload();
-      await blogPage.fillBlogForm( invalidUser, isReceiveNews );
-      expect.soft( await blogPage.formSubmitted.isVisible(), 'Invalid email format' ).not.toBeTruthy();
+      await blogPage.fillBlogForm( invalidUser );
+      expect.soft(page).toHaveURL(URL_TEST.BLOG);
+      expect( await blogPage.formSubmitted.isVisible(), 'Invalid email format' ).not.toBeTruthy();
     });
 
   });
